b9ba5b7b69bd895cdd8bee52ac58b7897b01862b
Ahmad M. AlShammari <dr.shammar@gmail.com>
2018-05-05 14:27:28 +0300

    Post Static life and detail blog

--- porto/settings.py
+++ porto/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILED_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'porto/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/j/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
